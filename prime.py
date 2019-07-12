f4f50323f33f24954cb3cf23695e77fa11a3554f
vijaiarivalagan <37081032+Vijaiarivalagan@users.noreply.github.com>
2019-07-12 16:20:54 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,7 +1,12 @@
 numers=list(map(int,input().split()))
 l=numers[0]
 r=numers[1]
-for i in range(l,r):
-  for j in range(2,i//2):
-    if(i%2!=0):
-      print(i)
+
+for num in range(l,r+1):
+   if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
+
