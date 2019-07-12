7ebcbaf9390719e008d6cd4c05313e30f38db6d1
vijaiarivalagan <37081032+Vijaiarivalagan@users.noreply.github.com>
2019-07-12 15:52:19 +0530

    Create camel_case.py

---
+++ camel_case.py
@@ -0,0 +1,9 @@
+st=input()
+print(st.title())
+
+'''string=list(input())
+string[0]=string[0].upper()
+for i in range((len(string))):
+  if(string[i]==' '):
+    string[i+1]=string[i+1].upper()
+print(''.join(string))'''
