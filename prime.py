f06950b24a995ed600171bf487508949a75aac76
vijaiarivalagan <37081032+Vijaiarivalagan@users.noreply.github.com>
2019-07-12 16:23:21 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,12 +1,12 @@
 numers=list(map(int,input().split()))
 l=numers[0]
 r=numers[1]
-
+count=0
 for num in range(l,r+1):
    if num > 1:
        for i in range(2,num):
            if (num % i) == 0:
                break
        else:
-           print(num)
-
+           count+=1
+print(count)
