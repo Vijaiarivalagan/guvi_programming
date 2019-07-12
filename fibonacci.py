5e322401123d717c4454020509da0f2d3bbf2f80
vijaiarivalagan <37081032+Vijaiarivalagan@users.noreply.github.com>
2019-07-12 15:31:02 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,14 @@
 num=int(input())
 a=1
 b=1
-fib_list=[1,1]
-for i in range(1,num-1):
-  c=a+b
-  a=b
-  b=c
-  fib_list.append(b)
-print(' '.join(str(i) for i in fib_list))
+if num==1:
+  print('1')
+
+else:
+  fib_list=[1,1]
+  for i in range(1,num-1):
+    c=a+b
+    a=b
+    b=c
+    fib_list.append(b)
+  print(' '.join(str(i) for i in fib_list))
