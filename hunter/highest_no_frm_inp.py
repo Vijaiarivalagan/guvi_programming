76d25377d1b10b8367deabca863596e1d5bc396e
vijaiarivalagan <37081032+Vijaiarivalagan@users.noreply.github.com>
2019-07-13 16:33:17 +0530

    Update and rename highest_no.py to highest_no_frm_inp.py

--- hunter/highest_no_frm_inp.py
+++ hunter/highest_no_frm_inp.py
@@ -2,4 +2,8 @@ total=int(input())
 #numbers=[int(input()) for i in range(total)]
 numbers=list(map(int,input().split()))
 numbers.sort(reverse=True)
-print(''.join(str(e) for e in numbers))
+num_str=''.join(str(e) for e in numbers)
+if(num_str[0]=='0'):
+  print("0")
+else:
+  print(num_str)
