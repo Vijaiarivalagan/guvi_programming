f39833fddf04dd43fb22324177705e6d1f5b6552
vijaiarivalagan <37081032+Vijaiarivalagan@users.noreply.github.com>
2019-07-13 14:49:40 +0530

    Update convert_min_cost.py

--- pro/convert_min_cost.py
+++ pro/convert_min_cost.py
@@ -3,6 +3,8 @@ length=len(st1) if len(st1)<len(st2) else len(st2)
 diff=abs(len(st1)-len(st2))
 count=diff
 for i in range(length):
+  if(len(st2)==1 and st2[i] in st1):
+    break
   if(st1[i]!=st2[i]):
     count+=1
 print(count)
