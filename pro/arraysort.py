ad71673362378bc73f9d1c33ec74010786b35902
vijaiarivalagan <37081032+Vijaiarivalagan@users.noreply.github.com>
2019-07-13 17:05:13 +0530

    Update arraysort.py

--- pro/arraysort.py
+++ pro/arraysort.py
@@ -1,9 +1,8 @@
-a=int(input())
-b=list(map(int,input().split()))
-l=[]
-for i in range(len(b)):
-    m=bin(b[i]).replace("0b"," ")
-    t.append(m)
-    t.sort(reverse=True)
-for j in l:
-    print(int(l,2))
+n1=int(input())
+arr1=list(map(int,input().split()))
+arr1=[bin(i) for i in arr1]
+arr1.sort(reverse=True)
+arr1=[int(x,2) for x in arr1]
+
+for i in range(0,n1):
+  print(arr1[i])
