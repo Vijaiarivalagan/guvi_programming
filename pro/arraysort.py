410410e6c37ca3139f41ac72b77ca446a36ac0dd
vijaiarivalagan <37081032+Vijaiarivalagan@users.noreply.github.com>
2019-07-13 16:39:08 +0530

    Create arraysort.py

---
+++ pro/arraysort.py
@@ -0,0 +1,9 @@
+a=int(input())
+b=list(map(int,input().split()))
+l=[]
+for i in range(len(b)):
+    m=bin(b[i]).replace("0b"," ")
+    t.append(m)
+    t.sort(reverse=True)
+for j in l:
+    print(int(l,2))
