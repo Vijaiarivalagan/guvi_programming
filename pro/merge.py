bbd743c68ead1d856430871a0833965373461478
vijaiarivalagan <37081032+Vijaiarivalagan@users.noreply.github.com>
2019-07-13 16:50:18 +0530

    Create merge.py

---
+++ pro/merge.py
@@ -0,0 +1,13 @@
+n=int(input())
+l=[]
+s=[]
+for i in range(0,n):
+  x=list(map(int,input().split()))
+  l.append(x)
+for i in l:
+  s+=i
+s.sort()
+new=""
+new=[str(i) for i in sum]
+res=" ".join(new)
+print(res)
